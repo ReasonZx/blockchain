f038d5833b31e8948f51f0e7f5cc3cad3a70ef25
ReasonZx <xico.reasonzx@gmail.com>
2022-04-18 23:41:53 +0200

    Added fundMe support for local chain

--- fundMe/scripts/deploy.py
+++ fundMe/scripts/deploy.py
@@ -1,9 +1,11 @@
+from ctypes import addressof
 from brownie import FundMe
-from scripts.helpfulScripts import getAccount
+from scripts.helpfulScripts import getAccount, getPriceFeedAddress
 
 def deployFundMe():
-    account     = getAccount()
-    fund_me     = FundMe.deploy({"from": account})
+    account             = getAccount()
+    priceFeedAddress    = getPriceFeedAddress(account)
+    fund_me             = FundMe.deploy( priceFeedAddress , {"from": account})
     print(f"Contract deployed to {fund_me.address}")
 
 def main():
